18a76c6545d36089b02fc99f4514b28b6f1d7b8a
Nicola Castellani <nicolacastellanidev@gmail.com>
2022-01-23 00:09:09 +0100

    Add new UI

--- SottoSopraGGJ22/Assets/Script/CreateAndJoinRooms.cs
+++ SottoSopraGGJ22/Assets/Script/CreateAndJoinRooms.cs
@@ -1,27 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
+
+    [SerializeField]
+    private GameObject LoadingUI = null;
+    
     public InputField createInput;
     public InputField joinInput;
 
 
     public void CreateRoom()
     {
-        PhotonNetwork.CreateRoom(createInput.text);
+        if (createInput.text == "")
+        {
+            return;
+        }
+        PhotonNetwork.JoinOrCreateRoom(joinInput.text, GetRoomConfig(), TypedLobby.Default);
+        
+        if (LoadingUI != null)
+        {
+            LoadingUI.SetActive(true);
+        }
     }
     
     public void JoinRoom()
     {
-        PhotonNetwork.JoinRoom(joinInput.text);
+        if (joinInput.text == "")
+        {
+            return;
+        }
+
+        PhotonNetwork.JoinOrCreateRoom(joinInput.text, GetRoomConfig(), TypedLobby.Default);
+        
+        if (LoadingUI != null)
+        {
+            LoadingUI.SetActive(true);
+        }
+    }
+
+    private RoomOptions GetRoomConfig()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        roomOptions.IsVisible = true;
+
+        return roomOptions;
     }
 
     public override void OnJoinedRoom()
     {
         PhotonNetwork.LoadLevel("Game");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        base.OnJoinRoomFailed(returnCode, message);
+        if (LoadingUI != null)
+        {
+            LoadingUI.SetActive(false);
+        }
     }
 }
