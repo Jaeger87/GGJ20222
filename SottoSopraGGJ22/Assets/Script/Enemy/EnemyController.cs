03cc89813f54a3d4e5f293c67b73255d57644ee9
Andrea <rosati.1595834@gmail.com>
2022-01-23 15:46:47 +0100

    Fix rete

--- SottoSopraGGJ22/Assets/Script/Enemy/EnemyController.cs
+++ SottoSopraGGJ22/Assets/Script/Enemy/EnemyController.cs
@@ -135,30 +135,32 @@ public class EnemyController : MonoBehaviour, IPunObservable
 
     public void OnStairsEnter(Vector3 i_StartPoint, Vector3 i_EndPoint, EStairPoint i_eStairPoint)
     {
-        if (i_eStairPoint == m_StairPointClimbStart)
+        if(PhotonNetwork.IsMasterClient)
         {
-            return;
-        }
+            if (i_eStairPoint == m_StairPointClimbStart)
+            {
+                 return;
+            }
         
-        if (m_bIsMovingOnStairs)
-        {
-            m_StairPointClimbStart = i_eStairPoint;
-            OnStairsExit();
-            return;
-        }
+            if (m_bIsMovingOnStairs)
+            {
+                 m_StairPointClimbStart = i_eStairPoint;
+                 OnStairsExit();
+                 return;
+            }
 
-        m_StairPointClimbStart = i_eStairPoint;
+             m_StairPointClimbStart = i_eStairPoint;
 
-        m_bFloorCheckEnabled = false;
+             m_bFloorCheckEnabled = false;
 
-        m_bIsMovingOnStairs = true;
-        m_Rigidbody.velocity = Vector2.zero;
+              m_bIsMovingOnStairs = true;
+              m_Rigidbody.velocity = Vector2.zero;
         
-            Vector3 LocalPosition = transform.localPosition;
-            LocalPosition.x = i_StartPoint.x;
-            transform.position = LocalPosition;
+              Vector3 LocalPosition = transform.localPosition;
+              LocalPosition.x = i_StartPoint.x;
+              transform.position = LocalPosition;
 
-            m_TargetStairDirection = i_EndPoint.y > transform.position.y ? Vector3.up : Vector3.down;
+              m_TargetStairDirection = i_EndPoint.y > transform.position.y ? Vector3.up : Vector3.down;
         }
     }
 
@@ -189,6 +191,7 @@ public class EnemyController : MonoBehaviour, IPunObservable
             transform.localPosition = LocalPosition;
             m_TargetStairDirection = Vector3.zero;
             m_bIsMovingOnStairs = false;
+            m_StairPointClimbStart = EStairPoint.None;
             m_TargetTeam = m_TargetTeam == ETeam.Team1 ? ETeam.Team2 : ETeam.Team1;
             SearchObjective();
         }
@@ -200,12 +203,19 @@ public class EnemyController : MonoBehaviour, IPunObservable
         if (stream.IsWriting)
         {
             stream.SendNext(m_Rigidbody.velocity);
+            stream.SendNext(m_bLookingRight);
         }
         else
         {
             //Network player, receive data
             m_Rigidbody.velocity = (Vector2)stream.ReceiveNext();
+            bool direction = (bool) stream.ReceiveNext();
+            if (direction != m_bLookingRight)
+            {
+                Flip();
+            }
             m_bvaluesReceived = true;
+            
         }
     }
 
