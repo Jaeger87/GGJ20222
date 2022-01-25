6cf0ad194344ef2f7069373fba0c1da8ec5c9df5
Andrea <rosati.1595834@gmail.com>
2022-01-25 22:45:41 +0100

    Spawn preview

--- SottoSopraGGJ22/Assets/Script/Enemy/EnemyController.cs
+++ SottoSopraGGJ22/Assets/Script/Enemy/EnemyController.cs
@@ -30,6 +30,8 @@ public class EnemyController : MonoBehaviour, IPunObservable
     [SerializeField]
     private AudioClip SpawnSound;
 
+    [SerializeField] private GameObject SpawnSprite;
+
     private ETeam m_TargetTeam = ETeam.Team1;
 
     private EnemyMovement m_Movement = EnemyMovement.Floor;
@@ -57,6 +59,7 @@ public class EnemyController : MonoBehaviour, IPunObservable
             m_Rigidbody.isKinematic = true;
         }
         m_PhotonView = GetComponent<PhotonView>();
+        SpawnSprite.SetActive(false);
     }
 
     private void Update()
@@ -210,20 +213,24 @@ public class EnemyController : MonoBehaviour, IPunObservable
         if (PhotonNetwork.IsMasterClient)
         {
             m_bIsDead = true;
-            StartCoroutine(AfterDeath(diePosition));
         }
+        
+        StartCoroutine(AfterDeath(diePosition));
         
     }
 
     private IEnumerator AfterDeath(Vector3 diePosition)
     {
-        yield return new WaitForSeconds(3f);
-        
         Vector2 LocalPosition = diePosition;
         
         float arenaXSize = MatchManager.GetMatchManager().ArenaSize.position.x;
         float arenaXSizeSigned = diePosition.x > 0 ? arenaXSize * -1 : arenaXSize;
-        LocalPosition.x = arenaXSizeSigned + diePosition.x ;
+        LocalPosition.x = arenaXSizeSigned + diePosition.x;
+
+        SpawnSprite.transform.position = LocalPosition;
+        SpawnSprite.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        SpawnSprite.SetActive(false);
         transform.localPosition = LocalPosition;
         
         m_TargetTeam = m_TargetTeam == ETeam.Team1 ? ETeam.Team2 : ETeam.Team1;
